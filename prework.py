60ab54764231305695655190893b077a4107b89d
Sar Champagne Bielert <sar@codepath.org>
2024-09-21 14:04:08 -0600

    Spacing tweak 3

--- prework.py
+++ prework.py
@@ -32,12 +32,10 @@ def check_challenge(challenge_file, solution_hash):
             print(f"✅ Correct!  The password is {file_content.decode('utf-8')}.")
             return True
         elif file_case_hash == solution_hash:
-            print("🚧 You're close, but something isn't quite right.")
-            print("Please check your capitalization and try again.")
+            print("🚧 You're close, but something isn't quite right. Please check your capitalization and try again.")
             return False
         elif file_strip_hash == solution_hash:
-            print("🚧 You're close, but something isn't quite right.")
-            print("Please check for extra spaces and try again.")
+            print("🚧 You're close, but something isn't quite right. Please check for extra spaces and try again.")
             return False
         else:
             print("⛔ Sorry, that's not the right answer.")
@@ -80,13 +78,10 @@ with open('applicant.txt', 'r') as file:
         c1, c2, c3 = False, False, False
         if response == 0 or response == 1:
             c1 = check_challenge("challenge_1.txt", "aca4fee155368758392aca3a58e5704c8650173981160328f27181fcf67b68e4")
-            print("\n")
         if response == 0 or response == 2:
             c2 = check_challenge("challenge_2.txt", "fd65204fde7b3236df176170aaffb6116533b241c30ce28b910f9872b3f9747b")
-            print("\n")
         if response == 0 or response == 3:
             c3 = check_challenge("challenge_3.txt", "6f93481fe2c111b1c44176bfa27e6461bf26668e0c6c31fab4a32fa8a4f663e5")
-            print("\n")
 
         if c1 and c2 and c3:
             with open('applicant.txt', 'rb') as file:
